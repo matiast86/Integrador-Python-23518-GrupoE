2ab9e7e504415786f9311b3612dac6c671e163a1
Matias Tailler <matiast86@gmail.com>
2023-10-24 11:56:09 -0300

    Contacto

--- assets/tp.js
+++ assets/tp.js
@@ -1,16 +1,19 @@
 function mostrarProductos() {
-    const seleccion = document.getElementById("cliente").value;
-    const productosEmpresas = document.getElementById("productos_empresas");
-    const productosIndividuos = document.getElementById("productos_individuos");
+  const seleccion = document.getElementById("cliente").value;
+  const productosEmpresas = document.getElementById("productos_empresas");
+  const productosIndividuos = document.getElementById("productos_individuos");
+
   
-    if (seleccion === "empresas") {
-      productosEmpresas.style.display = "block";
-      productosIndividuos.style.display = "none";
-    } else if (seleccion === "individuo") {
-      productosEmpresas.style.display = "none";
-      productosIndividuos.style.display = "block";
-    }
+  productosEmpresas.style.display = "none";
+  productosIndividuos.style.display = "none";
+
+  if (seleccion === "empresas") {
+    productosEmpresas.style.display = "block";
+  } else if (seleccion === "individuo") {
+    productosIndividuos.style.display = "block";
   }
+}
   
 
   mostrarProductos();
+
